e29b5ce05524a577bccd644172311d29d3cfb9a9
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-03-24 11:16:02 +0900

    feat: change model to mistral

--- index.ts
+++ index.ts
@@ -9,34 +9,27 @@ import { RunnableSequence } from "@langchain/core/runnables";
 
 const model = new Ollama({
   baseUrl: "http://localhost:11434", // Default value
-  model: "phi",
+  model: "mistral",
 });
 const prompt = ChatPromptTemplate.fromMessages([
   ["system", "You are a helpful chatbot"],
   new MessagesPlaceholder("history"),
   ["human", "{input}"],
 ]);
-
-// Default "inputKey", "outputKey", and "memoryKey values would work here
-// but we specify them for clarity.
 const memory = new BufferMemory({
   returnMessages: true,
 });
 
 await memory.loadMemoryVariables({});
 
-/*
-  { history: [] }
-*/
-
 const chain = RunnableSequence.from([
   {
-    input: (initialInput) => initialInput.input,
+    input: ({ input }) => input,
     memory: () => memory.loadMemoryVariables({}),
   },
   {
-    input: (previousOutput) => previousOutput.input,
-    history: (previousOutput) => previousOutput.memory.history,
+    input: ({ input }) => input,
+    history: ({ memory: { history } }) => history,
   },
   prompt,
   model,
@@ -53,35 +46,15 @@ console.log(response);
 await memory.saveContext(inputs, {
   output: response,
 });
-
-await memory.loadMemoryVariables({});
-
-/*
-  {
-    history: [
-      HumanMessage {
-        content: "Hey, I'm Bob!",
-        additional_kwargs: {}
-      },
-      AIMessage {
-        content: " Hi Bob, nice to meet you! I'm Claude, an AI assistant created by Anthropic to be helpful, harmless, and honest.",
-        additional_kwargs: {}
-      }
-    ]
-  }
-*/
+console.log(await memory.loadMemoryVariables({}));
 
 const inputs2 = {
   input: "What's my name?",
 };
-
 const response2 = await chain.invoke(inputs2);
 
 console.log(response2);
-
-/*
-  AIMessage {
-    content: ' You told me your name is Bob.',
-    additional_kwargs: {}
-  }
-*/
+await memory.saveContext(inputs, {
+  output: response2,
+});
+console.log(await memory.loadMemoryVariables({}));
